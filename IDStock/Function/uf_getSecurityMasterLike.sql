23ae97897926ad488167867438364f8884c5cfb2
Chochu <haxchochu@gmail.com>
2019-05-03 22:17:02 -0400

    Add uf_getSecurityMasterLike
    User Function to return SecurityMaster

---
+++ IDStock/Function/uf_getSecurityMasterLike.sql
@@ -0,0 +1,40 @@
+CREATE OR REPLACE FUNCTION public.uf_getSecurityMasterLike(
+	pSecurity character varying
+)
+     RETURNS TABLE (
+	 "SecurityName" character varying,
+	 "Region" character varying,
+	 "Startdate" date,
+	 "Symbol" character varying,
+	 "iexID" character varying,
+	 "StockType" character varying,
+	 "exchange" character varying,
+	 "currency" character varying,
+	 "isEnabled" boolean
+	) 
+    LANGUAGE 'plpgsql'
+AS $BODY$
+DECLARE var_search character varying;
+BEGIN
+	
+	var_search  :=  '%' || pSecurity || '%';
+
+	RETURN QUERY 
+	SELECT  "SecurityMaster"."SecurityName", 
+			"SecurityMaster"."Region", 
+			"SecurityMaster"."Startdate", 
+			"SecurityMaster"."Symbol", 
+			"SecurityMaster"."iexID", 
+			"SecurityMaster"."StockType", 
+			"SecurityMaster".exchange, 
+			"SecurityMaster".currency, 
+			"SecurityMaster"."isEnabled"
+	FROM public."SecurityMaster"
+	WHERE "SecurityMaster"."SecurityName" LIKE var_search
+		OR "SecurityMaster"."Symbol" LIKE var_search;
+     
+END; 
+$BODY$;
+
+ALTER FUNCTION public.uf_getSecurityMasterLike(character varying)
+    OWNER TO postgres;
